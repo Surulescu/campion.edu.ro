b23b060ca86ec10991cf67653becf1cfc2102d70
Surulescu <iliesurulescu@yahoo.com>
2016-11-07 20:51:07 +0200

    modificare

--- cifra1.c
+++ cifra1.c
@@ -5,33 +5,32 @@ int main()
     FILE *input,*output;
     input=fopen("cifra1.in","r");
     output=fopen("cifra1.out","w");
-    int n,m,k=0,q,i,j,temp;
+    int n,m,k=0,q,i,j,temp,l,r,z;
+    int a[100];
     fscanf(input,"%d",&n);
     fscanf(input,"%d",&m);
     temp=n;
     while(n>0)
     {
         q=n%10;
-        k=q+k;
+        k=k+q;
         n=n/10;
     }
-    if(k>m)
+    l=k-m+1;
+    q=0;
+    r=1;
+    z=0;
+    while(temp>0)
     {
-        for(i=k; i>=0; i--)
+        q=temp%10;
+        z=z+q;
+        for(i=r;i<=z;i++)
         {
-            q=temp%10;
-            for(j=k; j>=k-q; j--)
-            {
-                if(m==j)
-                {
-                    fprintf(output,"%d",q);
-                    j=k-q-1;
-                    i=-1;
-                }
-            }
-            temp=temp/10;
-            k=k-q;
+            a[i]=q;
+            if(i==l) fprintf(output,"%d",a[i]);
         }
+        r=z+1;
+        temp=temp/10;
     }
     fclose(input);
     fclose(output);
