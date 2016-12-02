54bb41d04220f9ec122d7b02c7e40645026e6bec
Surulescu <iliesurulescu@yahoo.com>
2016-12-02 21:12:55 +0200

    Add files via upload
    aceasi problema cu sirul de numere...

--- secvente2-copie.c
+++ secvente2-copie.c
@@ -1,44 +1,50 @@
 #include<stdio.h>
 int a[190001];
+int c[1000000];
+int b[1000000];
 
-int ordonare(int numar)
+void ordonare(int numar)
 {
-    int i,aux,stop=0;
-    while(stop==0)
+    int i,j,min=1000001,max=0;
+    for(i=numar; i>=1; i--)
     {
-        stop=1;
-        for(i=numar;i>=2;i--)
+        if(a[i]<min) min=a[i];
+        if(a[i]>max) max=a[i];
+    }
+    for(j=min; j<=max; j++) c[j]=0;
+    for(i=numar; i>=1; i--)
+    {
+        for(j=min; j<=max; j++)
         {
-            if(a[i]<a[i-1])
-            {
-                aux=a[i-1];
-                a[i-1]=a[i];
-                a[i]=aux;
-                stop=0;
-            }
+            if(a[i]==j) c[j]=1;
         }
     }
-    return;
+    for(j=min; j<=max; j++)
+    {
+        if(c[j]==1)  b[j]=j;
+    }
+    for(j=min; j<=max; j++) printf("%d ",b[j]);
+    printf("\n");
 }
 
 int numarare(int valoare)
 {
-    int i,temp=1,m1=1,r;
-    for(i=1;i<=valoare-1;i++)
+    int i,temp=1,m1=1;
+    for(i=valoare; i>=2; i--)
     {
-        if(a[i]-a[i+1]==-1) m1=m1+1;
+        //printf("%d ",a[i]);
+        //printf("%d\n",a[i-1]);
+        if(a[i]-a[i-1]==1)
+        {
+            m1=m1+1;
+            if(m1>temp) temp=m1;
+        }
         else
         {
-            if(m1>temp)
-            {
-                temp=m1;
-                m1=1;
-            }
+            m1=1;
         }
     }
-    if(temp==1) r=m1;
-    else r=temp;
-    return r;
+    return temp;
 }
 
 int main()
@@ -46,17 +52,14 @@ int main()
     FILE *input,*output;
     input=fopen("secvente2.in","r");
     output=fopen("secvente2.out","w");
-    int n,i,k,q=1;
+    int n,i;
     fscanf(input,"%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        fscanf(input,"%d",&a[i]);
-    }
+    for(i=1; i<=n; i++) fscanf(input,"%d",&a[i]);
     fprintf(output,"%d\n",1);
-    for(i=2;i<=n;i++)
+    for(i=2; i<=n; i++)
     {
         ordonare(i);
-        fprintf(output,"%d\n",numarare(i+1));
+        fprintf(output,"%d\n",numarare(i));
     }
     fclose(input);
     fclose(output);
