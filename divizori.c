1541a648b1e9a7df1e1f48f380e2feb9cb2d949e
Surulescu <iliesurulescu@yahoo.com>
2017-04-29 13:52:45 +0300

    a doua incercare
    second sol

--- divizori.c
+++ divizori.c
@@ -1,41 +1,50 @@
 #include<stdio.h>
 
+int cmmmc(int a,int b)
+{
+    int i,aux,stop,p,m;
+    aux=a*(a+1);
+    a=a+2;
+    for(i=a;i<b;i++)
+    {
+        stop=0;
+        m=aux;
+        p=2;
+        while(stop==0)
+        {
+            if(aux%i==0) stop=1;
+            else
+            {
+                aux=m*p;
+                p++;
+            }
+        }
+    }
+    return aux;
+}
+
+
 int main()
 {
     FILE *input,*output;
     input=fopen("divizori.in","r");
     output=fopen("divizori.out","w");
-    int m,k,N=0,intrare=1,aux,rest=0,oprire=0,temporar,repetari;
+    int m,k,N=0,x=0,nr_cautat,oprire=1;
     fscanf(input,"%d",&m);
     fscanf(input,"%d",&k);
-    repetari=m-k-1;
-    temporar=repetari;
-    N=m*m;
-    while(intrare==1)
+    k++;
+    nr_cautat=cmmmc(k,m);
+    while(oprire==1)
     {
-        aux=k;
-        repetari=temporar;
-        while(repetari>0)
+        x++;
+        N=x*nr_cautat+(k-1);
+        //printf("%d\n",N%m);
+        //printf("%d\n",N);
+        if(N%m==0)
         {
-            aux=aux+1;
-            rest=N%aux;
-            if(rest==k)
-            {
-                oprire=oprire+1;
-                repetari--;
-            }
-            else
-            {
-                repetari=0;
-                oprire=0;
-            }
-        }
-        if(oprire==temporar)
-        {
-            intrare=0;
+            oprire=0;
             fprintf(output,"%d",N);
         }
-        N=m+N;
     }
     return 0;
 }
